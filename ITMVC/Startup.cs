7a557cf8d3eb1fdcb6ce29a86d01016a6ca65571
dilanpiyananda1@outlook.com <dilanpiyananda1@outlook.com>
2021-04-13 04:11:35 -0700

    first commit

---
+++ ITMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITMVC.Startup))]
+namespace ITMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
