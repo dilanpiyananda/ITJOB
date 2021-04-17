b612ffb46e8aa470bf1abb38e69c69694872d4ae
dilanpiyananda1@outlook.com <dilanpiyananda1@outlook.com>
2021-04-17 07:43:38 -0700

    second commit

--- ITMVC/App_Start/BundleConfig.cs
+++ ITMVC/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ITMVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                       "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +24,9 @@ namespace ITMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/NavBar/navigation.css"));
+            //the following creates bundles in debug mode;
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
